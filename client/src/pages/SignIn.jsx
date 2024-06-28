6404cdc98de1393425a1b30403c671b1322f63e2
rishavs001 <risu55.2014@gmail.com>
2024-06-28 13:10:01 +0530

    Update SignIn.jsx

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -52,7 +52,7 @@ export default function SignIn() {
   return (
     <div className='bg-stone-100 shadow-md h-full'>
       <div className='p-3 pt-14 max-w-lg mx-auto'>
-        <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+        <h1 className='text-3xl text-center font-semibold my-9'>Sign In</h1>
         <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
           <input
             type='Email'
