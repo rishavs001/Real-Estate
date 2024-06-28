81502717ab622c31020a2728917298447132eb11
rishavs001 <risu55.2014@gmail.com>
2024-06-28 10:09:37 +0530

    about updated

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedin, FaRegEnvelope } from "react-icons/fa";
 
 export default function About() {
   return (
@@ -23,6 +24,24 @@ At NayaNivas, we understand that finding the perfect property or selling one can
         <li><strong className=' text-lime-600'>Comprehensive Support:</strong> Our dedicated support team is always ready to assist you at every step of your property journey, from listing to closing deals.</li>
         <li><strong className=' text-lime-600'>Innovative Solutions:</strong> We constantly strive to innovate and improve our platform to offer you the best real estate experience possible.</li>
       </ul>
+      <br/>
+      
+     
+      <div className="flex flex-col items-center justify-center mt-3 p-8 bg-lime-50 shadow-md rounded-lg">
+      <h1 className='text-3xl font-bold mb-1 text-lime-800'>About Developer</h1><br/>
+      <img src="https://rishavs001.github.io/image/rishav.jpg" alt="Rishav Shah" className="w-32 h-32 rounded-full mb-4" />
+      <h2 className="text-2xl text-slate-700 font-bold">Rishav Shah</h2>
+      <h3 className="text-blue-800">FULL Stack Developer</h3>
+      <p className="text-center text-slate-700 italic mt-2">"Combining tech innovation and real estate savvy to revolutionize your property journey."</p>
+      <div className="flex space-x-4 mt-4">
+        <a href="mailto:official.rishavs@gmail.com" target="_blank" rel="noopener noreferrer">
+          <FaRegEnvelope    />
+        </a>
+        <a href="https://www.linkedin.com/in/rishav97/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin/>
+        </a>
+      </div>
+    </div>
 
     </div>
     </div>
